9e9e69f60328b81986a6c813d8e74b25784883c1
Anders Ingemann <anders@ingemann.de>
2026-02-24 07:24:48 +0000

    Improve call structure, remove lanzaboote input

--- flake.nix
+++ flake.nix
@@ -4,21 +4,28 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default-linux";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-
   outputs =
-    { systems, flake-parts, ... }@inputs:
+    {
+      systems,
+      flake-parts,
+      ...
+    }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } (
-      { config, self, ... }@mkFlakeArgs:
+      {
+        flake-parts-lib,
+        config,
+        self,
+        ...
+      }@mkFlakeArgs:
+      let
+        inherit (flake-parts-lib) importApply;
+      in
       {
         systems = import systems;
         flake.nixosModules = {
           default = config.flake.nixosModules.install-nixos;
-          install-nixos = args: { imports = [ ((import ./nix mkFlakeArgs) args) ]; };
+          install-nixos = args: { imports = [ (importApply ./nix mkFlakeArgs) ]; };
         };
         perSystem =
           { pkgs, ... }:
