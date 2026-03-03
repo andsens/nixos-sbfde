8dde78420f011b00f18f590d0611ab0ab7e4992c
Anders Ingemann <anders@ingemann.de>
2026-03-03 21:53:45 +0000

    Fix option types for installer

--- nix/modules/installer.nix
+++ nix/modules/installer.nix
@@ -13,17 +13,17 @@ in
     enable = lib.mkEnableOption "installer ISO profile";
     repo.url = lib.mkOption {
       description = "URL of the installation repository";
-      type = lib.types.str;
+      type = lib.types.nullOr lib.types.str;
       default = null;
     };
     repo.deploy-key = lib.mkOption {
       description = "SSH private key that can access the installation repository";
-      type = lib.types.str;
+      type = lib.types.nullOr lib.types.str;
       default = null;
     };
     known_hosts = lib.mkOption {
       description = "Lines of known_hosts to add to the installer ISO SSH configuration, enables strict host key checking";
-      type = lib.types.lines;
+      type = lib.types.nullOr lib.types.lines;
       default = null;
     };
     package = lib.mkPackageOption self.packages.${pkgs.stdenv.hostPlatform.system} "installer" {
