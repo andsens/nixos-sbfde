99b5d05b141d699006ba292e020fe4a6f9133dd1
Anders Ingemann <anders@ingemann.de>
2026-06-14 16:10:44 +0200

    NixOS: Update to 26.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default-linux";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v1.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
--- nix/modules/default/secureboot.nix
+++ nix/modules/default/secureboot.nix
@@ -36,5 +36,22 @@ in
     system.fsPackages = lib.optional (lib.hasPrefix "ext"
       config.fileSystems."/".fsType
     ) pkgs.e2fsprogs; # Not automatically added because systemd-boot is "disabled"
+    systemd.services.copy-sb-keys-to-boot =
+      lib.mkIf (config.boot.lanzaboote.enable && config.boot.lanzaboote.autoGenerateKeys.enable)
+        {
+          restartIfChanged = true;
+          description = "Copy SecureBoot certificates to /boot, for manual installation";
+          unitConfig = {
+            After = [ "generate-sb-keys.service" ];
+            ConditionSecurity = [ "!uefi-secureboot" ];
+          };
+          serviceConfig.Type = "oneshot";
+          script = ''
+            ${lib.getExe' pkgs.coreutils "mkdir"} -p /boot/loader/keys/manual
+            ${lib.getExe pkgs.openssl} x509 -in "/${config.boot.lanzaboote.pkiBundle}/keys/db/db.pem" -outform der -out /boot/loader/keys/manual/db.cer
+            ${lib.getExe pkgs.openssl} x509 -in "/${config.boot.lanzaboote.pkiBundle}/keys/KEK/KEK.pem" -outform der -out /boot/loader/keys/manual/KEK.cer
+          '';
+          wantedBy = [ "generate-sb-keys.service" ];
+        };
   };
 }
